e10777ca72994c490780b9609c4e7342ea213051
akram mecheri <mecheri.akram@gmail.com>
2020-11-17 23:14:55 +0100

    page 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const { PORT=3000, LOCAL_ADDRESS='localhost' } = process.env
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, LOCAL_ADDRESS, () => console.log(`Listening on ${ PORT }`))
