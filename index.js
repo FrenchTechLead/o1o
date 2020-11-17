5af7ec671513fdd8b92e99bdebf9d7ee5cb4504b
akram mecheri <mecheri.akram@gmail.com>
2020-11-17 23:29:22 +0100

    try catch

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const path = require('path')
-const { PORT=3000, LOCAL_ADDRESS='localhost' } = process.env
+const express = require('express');
+const path = require('path');
+const { PORT=5000, LOCAL_ADDRESS='localhost' } = process.env;
 
-express()
+try {
+  express()
   .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, LOCAL_ADDRESS, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, LOCAL_ADDRESS, () => console.log(`Listening on http://${ LOCAL_ADDRESS }:${ PORT }`));
+} catch (error) {
+  console.error(error);
+}
+
